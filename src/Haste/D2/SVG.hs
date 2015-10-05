dd953d9975c3c5291ffc03c50bfb70e654aa95a3
Adam Washington <a.washington@sheffield.ac.uk>
2015-10-05 16:27:54 +0100

    Remove debugging prints

--- src/Haste/D2/SVG.hs
+++ src/Haste/D2/SVG.hs
@@ -14,7 +14,7 @@ type Line = JSAny
 -- | Creates a d3 line helper for translating between sets of data
 -- points and the SVG drawing commands.
 line :: Scale -> Scale -> IO Line
-line = ffi "function(x,y){return d3.svg.line().x(function(d,i){console.log('x',d,i);return x(d[0]);}).y(function(d,i){console.log('y',d,i);return y(d[1]);});}"
+line = ffi "function(x,y){return d3.svg.line().x(function(d,i){return x(d[0]);}).y(function(d,i){return y(d[1]);});}"
 
 -- | A simple helper function for creating the strings needed for the transform attribute of SVG.
 translate :: Point -> String
